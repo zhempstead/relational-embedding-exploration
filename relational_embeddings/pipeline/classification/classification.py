2e7940be7917c7b727dbdf647c1d325205ae1499
Zachary Hempstead <zhempstead@gmail.com>
2022-08-03 14:30:44 -0500

    another fix

--- relational_embeddings/pipeline/classification/classification.py
+++ relational_embeddings/pipeline/classification/classification.py
@@ -72,7 +72,8 @@ def classification(cfg, outdir, indir=None):
         'model': cfg.classification.methods,
     })
     df['dataset'] = cfg.dataset.name
-    for var, val in get_sweep_vars(outdir, cfg).items():
+    sweep_vars = get_sweep_vars(outdir, cfg)
+    for var, val in sweep_vars.items():
         df[var] = val
     df = df[['dataset'] + list(sweep_vars.keys) + ['model', 'pscore_train', 'pscore_test']]
     df.to_csv(outdir / 'results.csv', index=False)
